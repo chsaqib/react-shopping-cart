0f3d60c63addc465d40c2bb2f4429ab1e09a49d5
Saqib Nazir <saqib_9090@yahoo.com>
2021-09-22 12:45:55 +0100

    Shopping products list

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import Annoucement from '../components/Annoucement'
-import Categories from '../components/Categories'
-import Navbar from '../components/Navbar'
-import Slider from '../components/Slider'
+import React from 'react';
+import Annoucement from '../components/Annoucement';
+import Categories from '../components/Categories';
+import Navbar from '../components/Navbar';
+import Products from '../components/Products';
+import Slider from '../components/Slider';
 
 const Home = () => {
-    return (
-        <div>
-            <Annoucement/>
-            <Navbar/>
-            <Slider/>
-            <Categories/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Annoucement />
+      <Navbar />
+      <Slider />
+      <Categories />
+      <Products />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
