02fe4d0c5176a35570ef68493bc1f138e5c227cd
Saqib Nazir <saqib_9090@yahoo.com>
2021-09-23 00:20:47 +0100

    Shopping cart page design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 //import ProductList from './pages/ProductList';
 //import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Cart from './pages/Cart';
+//import Login from './pages/Login';
+//import Register from './pages/Register';
 //import Product from './pages/Product';
 const App = () =>{
-  return <Login/>
+  return <Cart/>
 }
 
 export default App;
